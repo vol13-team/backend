9cfc523a15003d0478ddfac76266303d9b102697
taku10101 <taku1011ht@gmail.com>
2024-02-16 15:26:50 +0900

    init

---
+++ src/domain/repository/user.repository.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll() {
+    try {
+      const users = await this.prisma.user.findMany();
+      return users;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async findOne(user_id: string) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { user_id: user_id },
+      });
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async create(dto: CreateUserDto) {
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          user_name: dto.user_name,
+          email: dto.email,
+        },
+      });
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async update(user_id: string, dto: UpdateUserDto) {
+    try {
+      const user = await this.prisma.user.update({
+        where: { user_id: user_id },
+        data: {
+          user_name: dto.user_name,
+          email: dto.email,
+        },
+      });
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async delete(user_id: string) {
+    try {
+      const user = await this.prisma.user.delete({
+        where: { user_id: user_id },
+      });
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
