f00384c2eee46e26e901a0fbd65816cad7e86eec
taku10101 <taku1011ht@gmail.com>
2024-03-18 12:54:38 +0900

    del

--- src/domain/repository/user.repository.ts
+++ src/domain/repository/user.repository.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+
 import { CreateUserDto } from "src/interface/dto/user.dto";
 @Injectable()
 export class AppService {
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,6 @@
-import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PrismaClient } from "@prisma/client";
 //DB接続ロジックを定義
-@Injectable()
 export class PrismaService extends PrismaClient {
   constructor(config: ConfigService) {
     super({
